362ce28066dd9a0c71cc1e6155875355b902e3cb
Fredomontero <christianmontero08@gmail.com>
2020-11-01 20:04:24 -0800

    Feat: Serve static files

--- src/app.js
+++ src/app.js
@@ -1,9 +1,22 @@
+const path = require('path');
 const express = require('express');
 
-const app = express(); 
 
-app.get('', (req, res) => {
-  res.send('Hello express!');
+const app = express(); 
+const publicDirectoryPath = path.join(__dirname, '../public/');
+
+app.use(express.static(publicDirectoryPath));
+
+app.get('/help', (req, res) => {
+  res.send('<h1>Help</h1>');
+});
+
+app.get('/about', (req, res) => {
+  res.send('<h1>About</h1>');
+});
+
+app.get('/weather', (req, res) => {
+  res.send('<h1>Weather</h1>');
 });
 
 // app.com

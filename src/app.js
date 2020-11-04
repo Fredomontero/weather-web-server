ece241ab07b1e1265cbbcdd0704eb3f8ac5c1049
Fredomontero <christianmontero08@gmail.com>
2020-11-03 20:41:39 -0800

    Feat: create footer partial

--- src/app.js
+++ src/app.js
@@ -20,31 +20,36 @@ app.use(express.static(publicDirectoryPath));
 
 app.get('', (req, res) => {
   res.render('index', {
-    title: "Home Page"
+    title: "Home Page",
+    name: "Christian Montero"
   });
 });
 
 app.get('/about', (req, res) => {
   res.render('about', {
     title: 'About page',
+    name: "Christian Montero"
   });
 });
 
 app.get('/help', (req, res) => {
   res.render('help', {
-    title: "Help page"
+    title: "Help page",
+    name: "Christian Montero"
   });
 });
 
 app.get('/help/*', (req, res) => {
   res.render('404', {
-    message: "Help article not found."
+    message: "Help article not found.",
+    name: "Christian Montero"
   });
 });
 
 app.get('*', (req, res) => {
   res.render('404', {
-    message: "Page not found."
+    message: "Page not found.",
+    name: "Christian Montero"
   });
 });
 

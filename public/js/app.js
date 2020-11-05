ebb1d6ed2f17c78580559658bc73e580e05581e2
Fredomontero <christianmontero08@gmail.com>
2020-11-04 21:39:04 -0800

    Feat: Show forecast and error message to frontend

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,22 @@
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
+let forecastMessage = document.querySelector('.forecastMessage');
+let forecastError = document.querySelector('.forecastError');
 
 weatherForm.addEventListener('submit', (e) => {
   e.preventDefault();
+  forecastMessage.textContent = 'Loading...';
+  forecastError.textContent = '';
   const location = search.value;
-  
   fetch(`http://localhost:3000/weather?address=${location}`)
   .then( response => {
     response.json().then((data) => {
       if(data.error){
-        console.log(data.error)
+        forecastError.textContent = data.error;
+        forecastMessage.textContent = '';
       }else{
-        console.log(data.location);
-        console.log(data.forecast);
+        forecastError.textContent = '';
+        forecastMessage.textContent = data.forecast;
       }
     });
   });
